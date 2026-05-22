b308b615fdd3b671c6b65c4cf226632b48544d7c
Patrick Fraley <patrick@fraley.de>
2026-05-22 10:09:45 +0200

    Move Libreoffice to Base

--- usr/libexec/lia-firstboot.sh
+++ usr/libexec/lia-firstboot.sh
@@ -7,13 +7,13 @@ flatpak remote-add --if-not-exists --system flathub \
 
 # Flatpaks systemweit installieren
 flatpak install -y --system flathub \
+    com.github.marhkb.Pods \
     com.github.tchx84.Flatseal \
     com.mattjakeman.ExtensionManager \
     io.github.kolunmi.Bazaar \
     io.github.linx_systems.ClamUI \
     org.gnome.Evolution \
     org.gnome.World.Secrets \
-    org.libreoffice.LibreOffice \
     org.onlyoffice.desktopeditors \
     org.gnome.Evolution \
     io.github.seadve.Kooha \
